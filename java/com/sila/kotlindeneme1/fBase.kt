06f914f4c01807f064717cee8f9215f6e5e3d1b1
silassnbr <102660195+silassnbr@users.noreply.github.com>
2023-03-29 14:26:00 +0300

    Refactoring

--- java/com/sila/kotlindeneme1/fBase.kt
+++
@@ -1,9 +0,0 @@
-package com.sila.kotlindeneme1
-
-import android.graphics.drawable.BitmapDrawable
-import android.os.Parcel
-import android.os.Parcelable
-import java.io.Serializable
-
-class fBase(val image: String? = "", val name: String? = "",val puan:Int?=0):Serializable,
-    BitmapDrawable(name)
